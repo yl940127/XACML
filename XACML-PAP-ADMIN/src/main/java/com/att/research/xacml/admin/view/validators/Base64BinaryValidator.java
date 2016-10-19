47ae368c8b72793961cfbdc5db34e592c90c77a6
Pamela Dragosh <pam.dragosh@gmail.com>
2016-10-19 15:53:06 -0400

    fix for non-thrown exception

--- XACML-PAP-ADMIN/src/main/java/com/att/research/xacml/admin/view/validators/Base64BinaryValidator.java
+++ XACML-PAP-ADMIN/src/main/java/com/att/research/xacml/admin/view/validators/Base64BinaryValidator.java
@@ -24,11 +24,22 @@ public class Base64BinaryValidator implements Validator {
 	@Override
 	public void validate(Object value) throws InvalidValueException {
 		if (value instanceof String) {
-			try {
-				new Base64().decode((String) value);
-			} catch (DecoderException e) {
-				throw new InvalidValueException(e.getLocalizedMessage());
-			}
+			// try {
+			new Base64().decode((String) value);
+		// Base64().decode(String) inherited from 
+		// org.apache.commons.codec.binary.BaseNCodec
+		// does not throw DecoderException in version 1.10
+		// of org.apache.commons.codec (the version we are
+		// using).  This may need to be uncommented in later
+		// versions.
+		//
+		// TODO - Since this does not throw an exception under
+		//        any circumstance, I question whether it is
+		//        a valid method of validating the input value.
+		//
+		// } catch (DecoderException e) {
+		// 	throw new InvalidValueException(e.getLocalizedMessage());
+		// }
 		} else
 			throw new InvalidValueException("Unrecognized Base64 Binary");
 	}

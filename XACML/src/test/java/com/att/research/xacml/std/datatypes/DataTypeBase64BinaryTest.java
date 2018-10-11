c6a4d4667b071cc9d560484771ec8460af380257
Pamela Dragosh <pdragosh@research.att.com>
2018-10-11 18:42:44 -0400

    Add JUnit coverage URI Boolean Base64

---
+++ XACML/src/test/java/com/att/research/xacml/std/datatypes/DataTypeBase64BinaryTest.java
@@ -0,0 +1,52 @@
+/*
+ *                        AT&T - PROPRIETARY
+ *          THIS FILE CONTAINS PROPRIETARY INFORMATION OF
+ *        AT&T AND IS NOT TO BE DISCLOSED OR USED EXCEPT IN
+ *             ACCORDANCE WITH APPLICABLE AGREEMENTS.
+ *
+ *          Copyright (c) 2018 AT&T Knowledge Ventures
+ *              Unpublished and Not for Publication
+ *                     All Rights Reserved
+ */
+
+package com.att.research.xacml.std.datatypes;
+
+import static org.junit.Assert.*;
+
+import java.util.Base64;
+
+import org.junit.Test;
+
+import com.att.research.xacml.api.DataTypeException;
+
+public class DataTypeBase64BinaryTest {
+
+	@Test
+	public void test() throws DataTypeException {
+		DataTypeBase64Binary datatype = DataTypeBase64Binary.newInstance();
+		
+		assertNull(datatype.convert(null));
+		
+		String test = "iamasecret";
+		Base64Binary base64 = Base64Binary.newInstance(test);
+		assertEquals(base64, datatype.convert(base64));
+		assertEquals(base64, datatype.convert(base64.getData()));
+		
+		String test64 = Base64.getEncoder().encodeToString(test.getBytes());
+		
+		System.out.println(test64);
+		
+		Base64Binary convertedObject = datatype.convert(test);
+		
+		System.out.println(convertedObject);
+		
+		System.out.println(convertedObject.getData());
+		
+	}
+	
+	@Test(expected = DataTypeException.class)
+	public void testConvert() throws DataTypeException {
+		DataTypeBase64Binary.newInstance().convert(new DataTypeTestObject());
+	}
+
+}
--- XACML/src/test/java/com/att/research/xacml/std/datatypes/DataTypeTestObject.java
+++ XACML/src/test/java/com/att/research/xacml/std/datatypes/DataTypeTestObject.java
@@ -1,3 +1,13 @@
+/*
+ *                        AT&T - PROPRIETARY
+ *          THIS FILE CONTAINS PROPRIETARY INFORMATION OF
+ *        AT&T AND IS NOT TO BE DISCLOSED OR USED EXCEPT IN
+ *             ACCORDANCE WITH APPLICABLE AGREEMENTS.
+ *
+ *          Copyright (c) 2018 AT&T Knowledge Ventures
+ *              Unpublished and Not for Publication
+ *                     All Rights Reserved
+ */
 package com.att.research.xacml.std.datatypes;
 
 public class DataTypeTestObject {
